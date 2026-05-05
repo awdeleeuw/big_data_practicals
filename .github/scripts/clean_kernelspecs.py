b42050dd19a2eee1a4c0148884344e361ca30c5e
awdeleeuw <a.de.leeuw@umcg.nl>
2026-05-05 13:12:56 +0200

    Minor changes

--- .github/scripts/clean_kernelspecs.py
+++ .github/scripts/clean_kernelspecs.py
@@ -2,13 +2,13 @@ import nbformat
 import glob
 
 for nb_path in glob.glob("**/*.ipynb", recursive=True):
-    with open(nb_path) as f:
+    with open(nb_path, encoding = "utf-8") as f:
         nb = nbformat.read(f, as_version=4)
     nb['metadata']['kernelspec'] = {
         "name": "python3",
         "display_name": "Python 3",
         "language": "python"
     }
-    with open(nb_path, 'w') as f:
+    with open(nb_path, 'w', encoding = "utf-8") as f:
         nbformat.write(nb, f)
 
